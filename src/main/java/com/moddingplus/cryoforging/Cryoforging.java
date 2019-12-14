b671d57d69087233d3303d9237c0d4c59df505bf
trikzon <trikzongaming@gmail.com>
2019-12-14 09:12:13 -0500

    Basic Forge Mod

---
+++ src/main/java/com/moddingplus/cryoforging/Cryoforging.java
@@ -0,0 +1,9 @@
+package com.moddingplus.cryoforging;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Cryoforging.MOD_ID)
+public class Cryoforging {
+
+    public static final String MOD_ID = "cryoforging";
+}
